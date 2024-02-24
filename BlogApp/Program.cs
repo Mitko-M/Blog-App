3497f9096622347f6ce5740d8e5981fca1d2c771
Mitko Mitkov <mitkomitkov2727@gmail.com>
2024-02-24 22:07:46 +0200

    Removed comments in Program.cs

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddAppServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -17,7 +16,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
