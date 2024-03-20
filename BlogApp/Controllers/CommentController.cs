5bdcbddf6159cb7456484abda42b9240c77d02fb
Mitko Mitkov <mitkomitkov2727@gmail.com>
2024-03-21 01:21:17 +0200

    Added Comment controller

---
+++ BlogApp/Controllers/CommentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class CommentController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
