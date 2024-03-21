6549d9445d03c452346f4d64d59e694afc59f68f
Mitko Mitkov <mitkomitkov2727@gmail.com>
2024-03-21 23:37:29 +0200

    Added and removed a test console app i wanted to try something

---
+++ temp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var directoryName = Path.GetFileName(currentDirectory);
+            var relativePath = directoryName.StartsWith("net6.0") ? @"../../../" : string.Empty;
+        }
+    }
+}
