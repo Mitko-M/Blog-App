48bbf2c23e6c6a66ce201de79d1260c6caed7243
Mitko Mitkov <mitkomitkov2727@gmail.com>
2024-03-31 00:05:23 +0200

    Added UserService with IsUserBanned method

---
+++ BlogApp.Core/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.Core.Contracts;
+using BlogApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Core.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly BlogAppDbContext _context;
+        public UserService(BlogAppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> IsUserBanned(string userId)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+            return user.Banned;
+        }
+    }
+}
